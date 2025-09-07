6e43891871695314724e9b3019f90ea5ec09975e
Sayyed-faiz-ali <156159566+Sayyed-faiz-ali@users.noreply.github.com>
2025-09-07 14:25:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,27 @@
 require('dotenv').config();
-
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-const authregister=require("./routes/auth")
-const resumeData= require("./routes/Data")
-const authMiddleware = require("./middleware/authmiddleware")
-const app = express();
 const path = require('path');
+
+const authregister = require('./routes/auth');
+const resumeData = require('./routes/Data');
+const authMiddleware = require('./middleware/authmiddleware');
+
+const app = express();
 connectDB();
 
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/auth', authregister);
+app.use('/api/data', authMiddleware, resumeData);
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-
-app.use('/api/auth',authregister);
-app.use('/api/data',authMiddleware,resumeData);
-
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
