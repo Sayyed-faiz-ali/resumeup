8140b6f28f289c65685ff80cc460c89b6372b74b
Sayyed-faiz-ali <156159566+Sayyed-faiz-ali@users.noreply.github.com>
2025-09-07 14:31:17 +0530

    Updates index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-const path = require('path');
 
 const authregister = require('./routes/auth');
 const resumeData = require('./routes/Data');
@@ -17,11 +16,6 @@ app.use(cors());
 app.use('/api/auth', authregister);
 app.use('/api/data', authMiddleware, resumeData);
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
