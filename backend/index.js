c75a0f37941f3297c4ff052c8f50f31d3266ec13
Sayyed-faiz-ali <156159566+Sayyed-faiz-ali@users.noreply.github.com>
2025-09-08 14:40:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,10 @@
-
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-
 const authregister = require('./routes/auth');
 const resumeData = require('./routes/Data');
 const authMiddleware = require('./middleware/authmiddleware');
-
 const path = require("path");
 
 const app = express();
@@ -16,20 +13,17 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 
-
-
-app.use(express.static(path.join(__dirname, "frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-});
+app.use('/api/auth', authregister);
+app.use('/api/data', authMiddleware, resumeData);
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
-app.use('/api/auth',authregister);
-app.use('/api/data', authMiddleware, resumeData);
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
