2fe8bac3d695316402bcbe1c68933f9bcbb1e691
Sayyed-faiz-ali <156159566+Sayyed-faiz-ali@users.noreply.github.com>
2025-09-08 14:54:42 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.get("/api/hello", (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
 });
 
